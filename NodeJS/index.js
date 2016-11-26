4164ce268817913f0f12b4c0c10d27e14a912f16
PauliusIvaskevicius <ivaskeviciuspaulius@gmail.com>
2016-11-26 18:33:00 +0100

    Update nodejs

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -9,8 +9,14 @@ var games = [];
 games.push({
 	"id":"jlHSgasgn",
 	"name":"TestGame",
+	"moderators":[],
 	"users": [],
-	"status":"running"
+	"status":"setup",
+	"questions":
+	[
+	{"id":0,"type":0,"question":"Is nodeJS cool","answer":"true","points":5},
+	{"id":1,"type":1,"question":"Whats the first NodeJS letter","answer":"N","points":25}
+	]
 })
 
 /*
@@ -19,19 +25,42 @@ GAME:
 	"id":"id",
 	"name":"Name",
 	"users": [],
-	"status":"running"
+	"status":"running",
+	questions:[]
 }
 
-USER:
+STATUS
+setup
+running
+ended
 
+QUESTION:
+{
+	"type":"",
+	"question":"",
+	"answer":""
+	"points":""
+}
+
+
+USER:
 {
 	"client":"",
 	"type":"",
 	"game":""
 }
 
+ANSWER
+{
+	"id":"",
+	"answer":"",
+	"type":""
+}
 
 
+TYPES 
+0 - True False
+1 - Enter Keyword
 
 */
 
@@ -48,14 +77,45 @@ io.on('connection', function(client)
     	submitanswer(client, {user:client["id"],answer:data});
 	});
 
+	client.on('joinGame', function (data) {
+    	console.log("Got join game : " + data);
+    	joingame(client, data, false);
+	});
+
+	client.on('moderateGame', function (data) {
+    	console.log("Got moderate game : " + data);
+    	joingame(client, data, true);
+	});
+
+	client.on('gameList', function () {
+    	console.log("Got game list request ");
+    	sendGameList(client);
+	});
+
+	client.on('endGame', function () {
+    	console.log("Got game list request ");
+    	endGame(client);
+	});
+
+	client.on('startgame', function (data) {
+    	console.log("Start game");
+    	startgame(client);
+	});
+
+	client.on('newQuestion', function (data) {
+    	console.log("New question " + data);
+    	displayNewQuestion(client,data);
+	});
+
+
+
 	client.on('register', function (data) {
 		if(!isUserRegistered(client))
 		{
 			console.log("New user " + simpleStringify(client));
-			users.push({"client":client,"type":data["type"]});
-			addUserToGame("jlHSgasgn", getUser(client));
-			displayArray();
+			users.push({"client":client,"points":0,"answeredQuestions":[]});
 			client.emit('registrationResponse', {status:"Success"});
+			sendGameList(client);
 		} 	
 		else
 		{
@@ -67,15 +127,225 @@ io.on('connection', function(client)
 
 io.listen(3000);
 
+function endGame(client)
+{
+	var user = getUser(client);
+	if(user == null || user["game"] == null || user["game"].length == 0)
+		return;
+
+	var game = getGame(user["game"]);
+	if(game == null || game["status"] != "running")
+		return;
+
+	if(isModerator(user,game))
+	{
+		game.status = "ended";
+		var Winner = AnalizeWinner(game);
+		sendMessageToAllGame(game, "gameResult", "Winner is " + Winner.id);
+		for(var i = 0; i < game.users.length; i++) 
+		{
+			game.users.game = null;
+			game.users.answeredQuestions = [];
+		}
+
+		for(var i = 0; i < game.moderators.length; i++)
+		{
+			game.moderators.game = null;
+			game.users.answeredQuestions = [];
+		}
+
+		for(var i = 0; i < users.length; i++)
+		{
+			sendGameList(users[i]["client"]);
+		}
+	}
+
+}
+
+function sendMessageToAllGame(game, type, message)
+{
+
+	for(var i = 0; i < game.users.length; i++)
+	{
+		game.users[i].client.emit(type,message);
+	}
+	for(var i = 0; i < game.moderators.length; i++)
+	{
+		game.moderators[i].client.emit(type,message);
+	}
+
+}
+
+function AnalizeWinner(game)
+{
+	var bestUser = game.users[0];
+	for(var i = 0; i < game.users.length; i++)
+	{
+		var User = game.users[i];
+		if(User.answeredQuestions.length > bestUser.answeredQuestions.length)
+			bestUser = User;
+	}
+	return bestUser;
+}
+
+function displayNewQuestion(client, data)
+{
+	var user = getUser(client);
+	if(user == null || user["game"] == null || user["game"].length == 0)
+		return;
+	console.log("User correct")
+	var game = getGame(user["game"]);
+
+	if(game == null || game["status"] != "running")
+		return;
+	console.log("Game correct")
+	if(isModerator(user,game))
+	{
+		console.log("User is  mod")
+		for(var i = 0; i < game["users"].length; i++)
+		{
+			console.log("Sending question " + game["questions"][data.questionId])
+			game["users"][i]["client"].emit('newQuestion', game["questions"][data.questionId]);
+		}
+	}
+
+}
+
+
+function isModerator(user, game)
+{
+	for(var i = 0; i < game["moderators"].length; i++)
+	{
+		if(user["client"] == game["moderators"][i]["client"])
+			return true;
+	}
+	return false;
+}
+
+function startgame(client)
+{
+	var user = getUser(client);
+	if(user == null || user["game"] == null)
+		return;
+
+	var game = getGame(user["game"]);
+	if(game == null)
+		return;
+
+
+	var userFound = false;
+	for(var i = 0; i < game["moderators"].length; i++)
+	{
+		var thisuser = game["moderators"][i];
+		if(thisuser["client"] == client)
+		{
+			userFound = true;
+			break;
+		}
+	}
+
+	if(userFound)
+	{
+		game.status = "running";
+		for(var i = 0; i < users.length; i++)
+		{
+			sendGameList(users[i]["client"]);
+		}
+	}
+
+}
+
+function sendGameList(client)
+{
+	var dataToSend = [];
+
+	for(var i = 0; i < games.length; i++)
+	{
+		var thisgame = games[i];
+		dataToSend.push({'id':thisgame['id'],'name':thisgame['name'],'status':thisgame['status']})
+	}
+	console.log(dataToSend)
+	client.emit('gameListResponse', dataToSend);
+	console.log("send")
+}
 
 function submitanswer(client, data) {
+
 	var user = getUser(client);
 	if(user == null) return;
 
 	var game = getGame(user["game"]);
 	if(game != null && game["status"] == "running")
+console.log(data)
+		console.log(data["answer"]["id"])
+	console.log(game["questions"]);
+
+	var question = game["questions"][data["answer"]["id"]];
+	
+	if(question == null)
+	{
+		console.log("Questio does not exist")
+		return;
+	}
+
+	console.log(question);
+
+	if(user.answeredQuestions.indexOf(question) > -1)
+		return;
+
+	if(question.type == 0 || question.type == 1 )
+	{
+		console.log("Tipas yra 0")
+
+		if(data.answer.answer == question.answer)
+		{
+			user.points += question.points;
+			user.client.emit('pointsUpdate',user.points);
+			user.answeredQuestions.push(question);
+		}
+		else
+		{
+			user.client.emit('submitanswerResponse', 'Wrong answer')
+		}
+	}
+
+
 
 	sendMessageToModerators(game["id"], data);
+}
+
+
+
+function joingame(client, data, moderate)
+{
+	var user = getUser(client);
+	if(user == null)
+	{
+		client.emit('joinGameResponse',{status:"You are not registered"})
+		return;
+	}
+
+	if(user["game"] != null && user["game"].length > 0)
+	{
+		client.emit('joinGameResponse',{status:"You are already in game"})
+		return;
+	}
+
+	var game = getGame(data["id"]);
+
+	if(game == null)
+	{
+		client.emit('joinGameResponse',{status:"Game does not exist"})
+		return;
+	}
+
+	if(game["status"] == "running" || game["status"] == "ended")
+	{
+		client.emit('joinGameResponse',{status:"Wrong game status : " + game["status"]})
+		return;
+	}
+
+	addUserToGame(data["id"], user, moderate);
 }
 
 function sendMessageToModerators(gameId, data) {
@@ -83,13 +353,12 @@ function sendMessageToModerators(gameId, data) {
 	var Game = getGame(gameId);
 	if(Game == null) return;
 	console.log("Game found. Searching for mods")
-	for(var i in Game["users"])
+	for(var i in Game["moderators"])
 	{
 			
-		var user = Game["users"][i];
+		var user = Game["moderators"][i];
 		console.log(user)
-		if(user["type"] == "moderator")
-		{
+
 
 			console.log("Found moderator, sending . " + user["client"]["id"]);
 			//io.clients[user["client"]["id"]].emit('moderatorOnSumbit',{data: data})
@@ -99,7 +368,7 @@ function sendMessageToModerators(gameId, data) {
     			io.sockets.connected[user["client"]["id"]].emit('moderatorOnSumbit',data)
 			}
 			console.log("Sent");
-		}
+		
 	}
 
 }
@@ -119,11 +388,12 @@ function getGame(id)
 	return null;
 }
 
-function addUserToGame(id, user)
+function addUserToGame(id, user, moderate)
 {
 
 		var Game = getGame(id);
-
+		if(!moderate)
+		{
 			var userAlreadyExists = false;
 			for(var j = 0; j < Game["users"].length;j++)
 			{
@@ -140,7 +410,31 @@ function addUserToGame(id, user)
 				user["game"] = id;
 				Game["users"].push(user);
 				console.log("User registered to game " + id);
-			}	
+				user["client"].emit('joinGameResponse',{status:"Success"})
+			}
+		}
+		else
+		{
+			var userAlreadyExists = false;
+			for(var j = 0; j < Game["moderators"].length;j++)
+			{
+				var GameUser = Game["moderators"][j];
+				if(GameUser["client"] == user["client"])
+				{
+					userAlreadyExists = true;
+					break;
+				}
+			}
+
+			if(!userAlreadyExists)
+			{
+				user["game"] = id;
+				Game.moderators.push(user);
+				console.log("User registered to game " + id);
+				user["client"].emit('joinGameResponse',{status:"Success"})
+			}
+
+		}			
 }
 
 
