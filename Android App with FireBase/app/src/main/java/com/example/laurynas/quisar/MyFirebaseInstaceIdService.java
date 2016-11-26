8188a2418b5cab8d388f24921487d35e2064adf3
Lauraudonius <lraudonius0@gmail.com>
2016-11-26 22:37:12 +0200

    Add firebase notifications functionality

---
+++ Android App with FireBase/app/src/main/java/com/example/laurynas/quisar/MyFirebaseInstaceIdService.java
@@ -0,0 +1,18 @@
+package com.example.laurynas.quisar;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Laurynas on 2016-11-26.
+ */
+public class MyFirebaseInstaceIdService extends FirebaseInstanceIdService {
+    private static final String REG_TOKEN = "REG_TOKEN";
+    @Override
+    public void onTokenRefresh() {
+        String recent_token = FirebaseInstanceId.getInstance().getToken();
+        Log.d(REG_TOKEN, recent_token);
+    }
+}
