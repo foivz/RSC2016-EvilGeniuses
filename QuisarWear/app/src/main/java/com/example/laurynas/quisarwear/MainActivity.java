4332af860f75927b7622c12e79efe1470736416d
Lauraudonius <lraudonius0@gmail.com>
2016-11-27 07:22:41 +0200

    Add AndroidWear App

---
+++ QuisarWear/app/src/main/java/com/example/laurynas/quisarwear/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.laurynas.quisarwear;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.wearable.activity.WearableActivity;
+import android.widget.TextView;
+
+public class MainActivity extends WearableActivity {
+    Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView timer = (TextView) findViewById(R.id.textView);
+        for(int i = 60;i >= 0;i++){
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                }
+            }, 1000);
+            timer.setText(String.valueOf(i));
+        }
+    }
+}
